a90852c48361cbeb22bf25fd2de38bdf5189c3ce
Zachary Pitts <zfp@protonmail.com>
2022-12-28 10:36:06 -0800

    adding cron shell script to reimport yasb data, and restart through pm2

---
+++ cron_yasb_reimport.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+cd /var/www/squad2xws
+
+rm src/services/yasb/lib/cards-common.js
+
+curl --fail https://yasb.app/javascripts/xwingcontent.min.js > js-holding/cards-common.js
+
+if [ -s /var/www/squad2xws/js-holding/cards-common.js ]
+then
+    echo "File not empty"
+    cat js-holding/cards-common.header js-holding/cards-common.js >> src/services/yasb/lib/cards-common.js
+
+    rm js-holding/cards-common.js
+
+    npx tsc
+
+    pm2 restart squad2xws
+else
+     echo "File empty"
+fi
